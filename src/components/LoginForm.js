f7fe8d3911eb1a4cb206bded9840a1e5a2327f34
Taylor Williams <taylor@thewilliams.net>
2020-03-22 19:29:10 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -10,6 +10,7 @@ class LoginForm extends React.Component {
   handleSubmit = (event) => {
     event.preventDefault()
     if (!this.state.username || !this.state.password) return
+    
     this.props.handleLogin(this.state)
   }
 
