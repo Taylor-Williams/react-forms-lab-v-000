a4c8a8ce82a2a418c07ea1e0229606f30768195c
Taylor Williams <taylor@thewilliams.net>
2020-03-22 18:37:37 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,21 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {remainingChars: this.props.maxChars};
+  }
+
+  countChars = (event) =>{
+    this.setState({
+      remainingChars: this.props.maxChars - this.target.count
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.countChars}/>
+        <p>characters remaining: {this.state.remainingChars}</p>
       </div>
     );
   }
